79b5b666b8f422e8c8fd8ad1a52265d27319588f
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-16 11:50:20 -0500

    Add redirections depending on the user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "home#index"
+  devise_scope :user do
+    authenticated :user do
+      root to: 'groups#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+   end
   
   # Routes for the categories and transaction pages.
   resources :groups, only: [:index, :show, :new, :create] do
