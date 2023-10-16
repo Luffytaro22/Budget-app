a241689cc877d243da2ade2a9ec230c5c83671c3
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-16 17:44:08 -0500

    Fixed linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
   end
 end
