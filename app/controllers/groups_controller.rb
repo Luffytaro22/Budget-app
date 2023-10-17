9889474af989e7d86e087471fbce87111c9ea02c
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-17 17:22:53 -0500

    Add new links for the categories images

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -33,19 +33,19 @@ class GroupsController < ApplicationController
 
   def load_photos
   	@photos = [
-      		"https://n9.cl/mgn495",
-      		"https://n9.cl/yko32",
-      		"https://n9.cl/z3rxl",
-      		"https://n9.cl/cxg32",
-    			"https://n9.cl/qmb20",
-    			"https://n9.cl/ysy5f",
-    			"https://n9.cl/1orfm",
-    			"https://n9.cl/60zpm",
-    			"https://n9.cl/v4c05",
-    			"https://n9.cl/nrm4c",
-    			"https://n9.cl/nufqes",
-    			"https://n9.cl/agfch",
-    			"https://n9.cl/rwoyb"
+      		"https://live.staticflickr.com/65535/48012754801_260dcc1070.jpg",
+      		"https://m.media-amazon.com/images/I/61M3SN0s6vL._AC_UF894,1000_QL80_.jpg",
+      		"https://img.freepik.com/vector-premium/dibujos-animados-electrodomesticos_24911-67071.jpg",
+      		"https://w0.peakpx.com/wallpaper/689/1023/HD-wallpaper-anime-original-computer-night.jpg",
+    			"https://media.gcflearnfree.org/content/5ccc48c7e5c6c4116cbd9df7_05_03_2019/consolasjuegos-01_xl.png",
+    			"https://cdn.pixabay.com/photo/2023/03/01/01/49/mountain-7822071_1280.jpg",
+    			"https://www.gob.mx/cms/uploads/article/main_image/22672/DEPORTES-01.jpg",
+    			"https://i.pinimg.com/originals/81/b2/34/81b234e6d4a79f243357e9f0d928135e.png",
+    			"https://png.pngtree.com/png-clipart/20230414/ourlarge/pngtree-beautiful-green-shoes-anime-style-illustration-png-image_6704693.png",
+    			"https://img.freepik.com/vector-gratis/pack-productos-limpieza-superficies_23-2148534089.jpg",
+    			"https://serviciostecnicosmovil.com/wp-content/uploads/2019/09/electronica.jpg",
+    			"https://static.wixstatic.com/media/802169_73480bec98d941f0891456532673c1b4~mv2.jpg/v1/fit/w_500,h_500,q_90/file.jpg",
+    			"https://www.respetmascotas.com/_Assets/img/181129-Imagen-AlimentacionMascotas.jpg"
       	]
   end
 end
