af7b6ed52578e6e87fb858707cc6e8e9ea1128a2
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-17 12:13:30 -0500

    Add new and create method actions in the group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,38 @@
 class GroupsController < ApplicationController
+	before_action :groups_by_current_user, only: [:new, :create]
+
   def index
     @groups = current_user.groups
+  end
+
+  def new
+  	@group = @groups.new
+  end
+
+  def create
+  	group = @groups.new(group_params)
+  	uploaded_io = params[:group][:icon]
+      File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+        file.write(uploaded_io.read)
+      end
+
+    group.icon = '/uploads/' + uploaded_io.original_filename
+  	if group.save
+  		flash[:notice] = 'The Category was created successfully!'
+  		redirect_to groups_path
+  	else
+  		flash[:alert] = group.errors.full_messages.join(', ')
+  		redirect_to request.referrer
+  	end
+  end
+
+  private
+
+  def group_params
+  	params.require(:group).permit(:name, :icon)
+  end
+
+  def groups_by_current_user
+  	@groups = current_user.groups
   end
 end
