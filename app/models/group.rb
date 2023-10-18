dc9db8ebaffc31f9c5a556dcc5dc92471ae76fbf
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-10-18 09:48:44 -0500

    Add validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,10 @@ class Group < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :purchases, join_table: 'groups_purchases'
 
+  # Validations
+  validates :name, :icon, presence: true
+  validates :name, length: { maximum: 20 }
+
   def sum_amount
     purchases.sum(&:amount)
   end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,9 @@
 class Purchase < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :groups, join_table: 'groups_purchases'
+
+  # Validations
+  validates :name, :amount, presence: true
+  validates :name, length: { maximum: 30 }
+  validates :amount, numericality: { greater_than_or_equal_to: 0 }
 end
